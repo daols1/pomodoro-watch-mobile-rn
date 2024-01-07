e804374b4df2062fa73be160c8bd7e4b78a5e8bf
David <78326757+daols1@users.noreply.github.com>
2024-01-07 17:05:57 +0100

    Initial folder structure

---
+++ app/_layout.js
@@ -0,0 +1,12 @@
+import { StatusBar } from 'expo-status-bar';
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <SafeAreaView>
+      <Text className='text-3xl' >Open up App.js to start working on your app! in layout.js!!!?? </Text>
+      <StatusBar style="auto" />
+    </SafeAreaView>
+  );
+}
+
